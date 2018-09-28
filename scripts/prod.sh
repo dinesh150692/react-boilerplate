5d13abfbd773a34a45c95b9e10c100552ad73d9f
Dinesh kumar D <dinesh@innoventes.co>
2018-09-28 15:49:12 +0530

    Made Modification for the prod script

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,3 +1,3 @@
-cross-env NODE_ENV=production webpack
+cross-env NODE_ENV=production webpack --config webpack/webpack.config.js
 
 sed -i "" -e "s|<script type=\"text/javascript\"|<script defer text=\"text/javascript\" |g" build/index.html
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,4 @@
-const common = require('./webpack/webpack.common');
+const common = require('./webpack.common');
 const webpackMerge = require('webpack-merge');
 
 const envs = {
@@ -6,5 +6,5 @@ const envs = {
   production: 'prod',
 };
 const env = envs[process.env.NODE_ENV || 'development'];
-const envConfig = require(`./webpack/webpack.${env}.js`);
+const envConfig = require(`./webpack.${env}.js`);
 module.exports = webpackMerge(common, envConfig);
