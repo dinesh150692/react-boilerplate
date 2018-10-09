33c7eb66a34c366860af552a881f7b05d044a21a
Dinesh kumar D <dinesh@innoventes.co>
2018-10-09 11:06:45 +0530

    Fixed Issue in the development mode

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,13 +1,27 @@
-
 const commonPaths = require('./paths');
 module.exports = {
   entry: commonPaths.entryPath,
+  resolve: {
+    modules: ['src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.css', '.scss'],
+    alias: {
+      //enable this comment for moving from react to preact
+      // "react": "preact-compat",
+      // "react-dom": "preact-compat"
+      "Components": commonPaths.root + '/src/components/',
+      "Pages": commonPaths.root + '/src/pages/',
+      "Actions": commonPaths.root + '/src/redux/actions',
+      "ActionType": commonPaths.root + '/src/redux/actionType',
+      "Sagas": commonPaths.root +  '/src/redux/sagas',
+      "Reducers": commonPaths.root + '/src/redux/reducers'
+    }
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader'
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -33,29 +47,11 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    modules: ['src', 'node_modules'],
-    extensions: ['*', '.js', '.jsx', '.css', '.scss'],
-  },
   node: {
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
     dgram: 'empty',
     child_process: 'empty'
-  },
-  
-  resolve: {
-    alias: {
-      //enable this comment for moving from react to preact
-      // "react": "preact-compat",
-      // "react-dom": "preact-compat"
-      "Components": commonPaths.root + '/src/components/',
-      "Pages": commonPaths.root + '/src/pages/',
-      "Actions": commonPaths.root + '/src/redux/actions',
-      "ActionType": commonPaths.root + '/src/redux/actionType',
-      "Sagas": commonPaths.root +  '/src/redux/sagas',
-      "Reducers": commonPaths.root + '/src/redux/reducers'
-    }
   }
 };
